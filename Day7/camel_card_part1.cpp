61c1cd1317c1449dc1ebffa9ccc907e520c4cfa4
jedshady <wendelljiang.au@gmail.com>
2023-12-13 22:38:56 +1100

    Fix Day 7 to read in file from command line

--- Day7/camel_card_part1.cpp
+++ Day7/camel_card_part1.cpp
@@ -59,7 +59,7 @@ void classifyHand2Type(map<string, vector<pair<string, int>>> &type2hand, const 
     
     string hand_type = determineType(current_hand);
 
-    cout << "Current hand: " << current_hand << " Current bid: " << current_bid << " Type: " << hand_type << endl;
+    // cout << "Current hand: " << current_hand << " Current bid: " << current_bid << " Type: " << hand_type << endl;
 
     type2hand[hand_type].push_back(make_pair(current_hand, current_bid));
 }
@@ -105,8 +105,8 @@ int sortHandAndGetSum(vector<pair<string, int>> &hand_in_type, const int startin
     return sum;
 }
 
-int main() {
-    ifstream file("original_input");
+int main(int argc, char* argv[]) {
+    ifstream file(argv[1]);
     string line;
 
     map<string, vector<pair<string, int>>> type2hand; 
@@ -140,49 +140,49 @@ int main() {
         winning_high_card = sortHandAndGetSum(type2hand["High_card"], current_idx + 1);
         current_idx += type2hand["High_card"].size(); 
     }
-    cout << "Winning from High Card: " << winning_high_card << endl;
+    // cout << "Winning from High Card: " << winning_high_card << endl;
 
     if (type2hand.find("One_pair") != type2hand.end()) {
         winning_one_pair = sortHandAndGetSum(type2hand["One_pair"], current_idx + 1);
         current_idx += type2hand["One_pair"].size(); 
     }
-    cout << "Winning from One Pair: " << winning_one_pair << endl;
+    // cout << "Winning from One Pair: " << winning_one_pair << endl;
 
     if (type2hand.find("Two_pair") != type2hand.end()) {
         winning_two_pair = sortHandAndGetSum(type2hand["Two_pair"], current_idx + 1);
         // cout << "current idx: " << current_idx << endl;
         current_idx += type2hand["Two_pair"].size(); 
     }
-    cout << "Winning from Two Pair: " << winning_two_pair << endl;
+    // cout << "Winning from Two Pair: " << winning_two_pair << endl;
 
     if (type2hand.find("Three_of_a_kind") != type2hand.end()) {
         winning_three_of_a_kind = sortHandAndGetSum(type2hand["Three_of_a_kind"], current_idx + 1);
         current_idx += type2hand["Three_of_a_kind"].size(); 
     }
-    cout << "Winning from Three of a Kind: " << winning_three_of_a_kind << endl;
+    // cout << "Winning from Three of a Kind: " << winning_three_of_a_kind << endl;
 
     if (type2hand.find("Full_house") != type2hand.end()) {
         winning_full_house = sortHandAndGetSum(type2hand["Full_house"], current_idx + 1);
         current_idx += type2hand["Full_house"].size(); 
     }
-    cout << "Winning from Full House: " << winning_full_house << endl;
+    // cout << "Winning from Full House: " << winning_full_house << endl;
 
     if (type2hand.find("Four_of_a_kind") != type2hand.end()) {
         winning_four_of_a_kind = sortHandAndGetSum(type2hand["Four_of_a_kind"], current_idx + 1);
         current_idx += type2hand["Four_of_a_kind"].size(); 
     }
-    cout << "Winning from Four of a Kind: " << winning_four_of_a_kind << endl;
+    // cout << "Winning from Four of a Kind: " << winning_four_of_a_kind << endl;
 
     if (type2hand.find("Five_of_a_kind") != type2hand.end()) {
         winning_five_of_a_kind = sortHandAndGetSum(type2hand["Five_of_a_kind"], current_idx + 1);
         current_idx += type2hand["Five_of_a_kind"].size(); 
     }
-    cout << "Winning from Five of a Kind: " << winning_five_of_a_kind << endl;
+    // cout << "Winning from Five of a Kind: " << winning_five_of_a_kind << endl;
 
     total_winning = winning_high_card + winning_one_pair + winning_two_pair + winning_three_of_a_kind +
         winning_full_house + winning_four_of_a_kind + winning_five_of_a_kind;
     
-    cout << "Total Winning: " << total_winning << endl;
+    cout << "Part 1 total winning: " << total_winning << endl;
 
     return 0;
 }
