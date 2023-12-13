02d64cd8b0aa0d8f3a2fd452bc034914d2b2dfbf
jedshady <wendelljiang.au@gmail.com>
2023-12-13 22:57:53 +1100

    Fix Day 10

--- Day10/pipe_maze.py
+++ Day10/pipe_maze.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3 -u
 
+import sys
+from datetime import datetime
 
 def main():
-    filename = "original_input"
+    filename = sys.argv[1]
     
     grid: list(str) = list()
     
@@ -20,8 +22,8 @@ def main():
     # print(grid)
     # print(start_point)
 
-    mid_way = 0
     trace = list()
+    points = [start_point]
     cur_point = start_point
 
     found_path = False
@@ -33,7 +35,7 @@ def main():
         if grid[x][y] in ('|', '7', 'F'):
             cur_point = [x, y]
             found_path = True
-            trace.append('U') 
+            trace.append('U')
     
     if not found_path and cur_point[1] + 1 <= len(grid[0]):
         # print("Checking right")
@@ -65,6 +67,7 @@ def main():
     # print(cur_point)
 
     while grid[cur_point[0]][cur_point[1]] != 'S':
+        points.append(cur_point)
         nav = grid[cur_point[0]][cur_point[1]]
         # print(f"nav is {nav}")
         last_move = trace[-1]
@@ -110,11 +113,35 @@ def main():
             if last_move == 'L':
                 cur_point = [cur_point[0]-1, cur_point[1]]
                 trace.append('U')
-    
+
+
     # print(trace)
+    # print(points)
+
+    # Part 1
     mid_way = int(len(trace) / 2)
-    print(f"mid way is {mid_way}")
-            
+    print(f"Part 1: mid way is {mid_way}")
+
+    # Part 2
+    # Shoelace formula: https://en.wikipedia.org/wiki/Shoelace_formula
+    # Need to take absolute value because calculations clock-wise and anti-clock-wise is opposite to each other
+    padded_points = [*points, points[0]]  # append the start point
+    area = abs(sum(
+            row1 * col2 - row2 * col1
+            for (row1, col1), (row2, col2) in zip(padded_points, padded_points[1:])
+        ) / 2)
+
+    # print(padded_points)
+    # print(abs(area))
+
+    # Pick's theorem: https://en.wikipedia.org/wiki/Pick%27s_theorem
+    num_interior_points = int(abs(area) - 0.5 * len(points) + 1)
+    print(f"Part 2: number of interior point is {num_interior_points}")
+
 
 if __name__ == "__main__":
+    # start = datetime.now()
     main()
+    # end = datetime.now()
+    # elapsed = (end - start).microseconds / 1000 
+    # print(f"Whole process took {elapsed}ms")
